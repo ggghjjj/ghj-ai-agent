976cdf945607a20e99ab3b58352677b265703435
JasonGao <jasongao@jiemogame.com>
2025-05-15 18:57:12 +0800

    fist commit

---
+++ src/main/java/com/ghj/ghjaiagent/GhjAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.ghj.ghjaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhjAiAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhjAiAgentApplication.class, args);
+	}
+
+}
